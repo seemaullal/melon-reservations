3c0a0ea480edc97ce2b16156d06382b513bf58b1
Seema Ullal <seema.ullal@afterpay.com>
2021-08-25 08:33:48 -0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { Fragment } from "react";
+import { AppBar, Toolbar, Typography, CssBaseline } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import ScheduleIcon from "@material-ui/icons/Schedule";
 
 function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("/api/test-data")
-      .then((response) => response.json())
-      .then((testData) => setData(testData));
-  });
+  const useStyles = makeStyles((theme) => ({
+    icon: {
+      marginRight: theme.spacing(2),
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div>
-      {data.map((item) => (
-        <p>{item}</p>
-      ))}
-    </div>
+    <Fragment>
+      <CssBaseline />
+      <AppBar position="absolute">
+        <Toolbar>
+          <ScheduleIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Melon Tasting Scheduler
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Fragment>
   );
 }
 
