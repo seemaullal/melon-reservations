d00b25d01e2c0b5a872202df5eb6baeafa36c4ff
Seema Ullal <seema.ullal@afterpay.com>
2021-08-25 05:30:48 -0700

    make sure frontend can hit backend server, fetch data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("/api/test-data")
+      .then((response) => response.json())
+      .then((testData) => setData(testData));
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.map((item) => (
+        <p>{item}</p>
+      ))}
     </div>
   );
 }
