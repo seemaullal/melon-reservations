3e7e9103d177b08bd61a5dfce61aea34aca72ef6
Seema Ullal <seema.ullal@afterpay.com>
2021-08-25 09:36:43 -0700

    add component for schedule, routing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
 import "./App.css";
-import { Fragment } from "react";
-import { AppBar, Toolbar, Typography, CssBaseline } from "@material-ui/core";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link as RouterLink,
+} from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  CssBaseline,
+  Button,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import ScheduleIcon from "@material-ui/icons/Schedule";
+import Schedule from "./components/Schedule";
 
 function App() {
   const useStyles = makeStyles((theme) => ({
+    appBar: {
+      marginBottom: theme.spacing(2),
+    },
     icon: {
       marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
     },
   }));
 
   const classes = useStyles();
 
   return (
-    <Fragment>
+    <BrowserRouter>
       <CssBaseline />
-      <AppBar position="absolute">
+      <AppBar position="static" className={classes.appBar}>
         <Toolbar>
           <ScheduleIcon className={classes.icon} />
-          <Typography variant="h6" color="inherit" noWrap>
+          <Typography
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
             Melon Tasting Scheduler
           </Typography>
+          <Button to="/schedule" component={RouterLink} color="inherit">
+            Schedule Tasting
+          </Button>
         </Toolbar>
       </AppBar>
-    </Fragment>
+      <Switch>
+        <Route path="/schedule">
+          <Schedule />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Schedule.jsx
@@ -0,0 +1,9 @@
+import { Container } from "@material-ui/core";
+
+export default function Schedule() {
+  return (
+    <Container maxWidth="lg">
+      <div>Schedule</div>
+    </Container>
+  );
+}
