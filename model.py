fb08c2b3392e3879c137011f14e37594082dd7c1
Seema Ullal <seema.ullal@afterpay.com>
2021-09-05 08:08:43 -0700

    try to fix track modifications issue

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ class Reservation(db.Model):
 def connect_to_db(flask_app, db_uri, echo):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
-    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.app = flask_app
     db.init_app(flask_app)
