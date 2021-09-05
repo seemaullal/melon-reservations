aa6461359c62c343cee8d3b2998f7ab3bab48dd1
Seema Ullal <seema.ullal@afterpay.com>
2021-09-05 15:25:55 -0700

    fix routing issue

--- api.py
+++ api.py
@@ -85,6 +85,10 @@ def search_reservation():
 def index(path):
     return app.send_static_file("index.html")
 
+@app.errorhandler(404)
+def not_found(_error):
+    return app.send_static_file("index.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
